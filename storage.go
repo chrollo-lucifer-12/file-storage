9c64aa0fde6bcaab43073b3692f0393e73641c32
sahil-singh1107 <sahilpython@gmail.com>
2025-10-05 20:56:56 +0530

    init storage

---
+++ storage.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+type PathTransformFunc func(string) string
+
+var DefaultPathTransformFunc = func(key string) string {
+	return key
+}
+
+type StoreOpts struct {
+	PathTransformFunc PathTransformFunc
+}
+
+type Store struct {
+	StoreOpts
+}
+
+func NewStore(opts StoreOpts) *Store {
+	return &Store{
+		StoreOpts: opts,
+	}
+}
+
+func (s *Store) writeStream(key string, r io.Reader) error {
+	pathname := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathname, os.ModePerm); err != nil {
+		return err
+	}
+
+	fileName := "random"
+	pathAndFileName := pathname + "/" + fileName
+	f, err := os.Create(pathAndFileName)
+	if err != nil {
+		return err
+	}
+
+	n, err := io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk: %s", n, pathAndFileName)
+
+	return nil
+}
